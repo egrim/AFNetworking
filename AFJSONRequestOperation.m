acecf5cd3e173cd99193f2475928862c2ee8d894
Mattt Thompson <m@mattt.me>
2011-09-16 10:36:36 -0500

    Stashing XML Request Operation

--- AFJSONRequestOperation.m
+++ AFJSONRequestOperation.m
@@ -28,7 +28,7 @@
 static dispatch_queue_t af_json_request_operation_processing_queue;
 static dispatch_queue_t json_request_operation_processing_queue() {
     if (af_json_request_operation_processing_queue == NULL) {
-        af_json_request_operation_processing_queue = dispatch_queue_create("com.alamofire.json-request.processing", 0);
+        af_json_request_operation_processing_queue = dispatch_queue_create("com.alamofire.networking.json-request.processing", 0);
     }
     
     return af_json_request_operation_processing_queue;
